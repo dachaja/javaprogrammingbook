191edd95e05b8597f92315f1774fa2db94a8c140
dachaja <pettitbusiness@gmail.com>
2014-11-24 14:08:51 -0800

    observer design pattern

---
+++ DesignPattern/src/com/lookcapp/observer/DigitObserver.java
@@ -0,0 +1,14 @@
+package com.lookcapp.observer;
+
+public class DigitObserver implements Observer{
+
+	@Override
+	public void update(NumberGenerator generator) {
+		System.out.println("DigitObserver: " + generator.getNumber());
+		try {
+			Thread.sleep(100);
+		} catch (Exception e) {
+		}
+	}
+
+}
---
+++ DesignPattern/src/com/lookcapp/observer/Observer.java
@@ -0,0 +1,5 @@
+package com.lookcapp.observer;
+
+public interface Observer {
+	public abstract void update(NumberGenerator generator);
+}
